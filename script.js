5666e8788a6499fe21449a06710eb4ba9572a5ec
mrlaboratory <admin@mrlaboratory.com>
2020-09-15 14:16:21 +0600

    modified html css and js file for addeding effect name

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 var imgs = document.querySelector("img");
-
+var text1 = document.querySelector(".side-top p");
 var effect = ["effect0","effect1","effect2","effect3","effect4","effect5","effect6","effect7","effect8"];
 var bttn = document.querySelectorAll("button");
 
-/*for(var i=0;i<8;i++){
-  
-document.querySelectorAll("button")[i].addEventListener("click" , function(){ 
-   text = document.querySelectorAll("button")[i];
-alert(text);
-   imgs.classList.add(effect[i]);
+
+for(var i=0;i<bttn.length;i++){
+  document.querySelectorAll("button")[i].addEventListener("click" , function(){ 
+   
+   text1.innerHTML=this.innerHTML;
+
         
         });
 
 }
-*/
-function eff(num){
-    imgs.className ='effect'+num;
 
+function eff(num){  
+    imgs.className ='effect'+num;
+   
   
 }
 
@@ -24,6 +24,8 @@ $(function(){
    $('#myFile').change(function(){
      var input = this;
      var url = $(this).val();
+  
+     
      var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
      if (input.files && input.files[0]&& (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) 
       {
